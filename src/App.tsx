788c4084fdfec22f00c15a53bae191de192cac4f
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-10 23:13:31 +0300

    feat: added typescript and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import {Header} from './components/Header';
+import {Main} from './components/Main';
+
+import {HomePage} from './pages/HomePage';
+import {Details} from './pages/Details';
+import {NotFound} from './pages/NotFound';
 
 export const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Main>
+                <Routes>
+                    <Route path="/" element={
+                        <HomePage/>
+                    }/>
+                    <Route path="/country/:name" element={<Details/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Main>
+        </>
+    );
 }
-
