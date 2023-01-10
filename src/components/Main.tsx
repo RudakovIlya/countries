a9ea48400eb7d27d064c378101d4892c06ab8cfc
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-10 23:14:25 +0300

    feat: added new components

---
+++ src/components/Main.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import {Container} from './Container';
+import {FC, ReactElement} from "react";
+
+const Wrapper = styled.main`
+  padding: 2rem 0;
+
+  @media (min-width: 767px) {
+    padding: 4rem 0;
+  }
+`;
+
+type MainPropsType = {
+    children: ReactElement
+}
+
+export const Main: FC<MainPropsType> = ({children}) => {
+    return (
+        <Wrapper>
+            <Container>{children}</Container>
+        </Wrapper>
+    );
+};
---
+++ src/components/Search.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+import {IoSearch} from 'react-icons/io5';
+import {FC} from "react";
+
+const InputContainer = styled.label`
+  background-color: var(--colors-ui-base);
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
+
+  border-radius: var(--radii);
+  box-shadow: var(--shadow);
+  width: 100%;
+  margin-bottom: 1rem;
+
+  @media (min-width: 767px) {
+    margin-bottom: 0;
+    width: 280px;
+  }
+`;
+
+const Input = styled.input.attrs({
+    type: 'search',
+    placeholder: 'Search for a country...',
+})`
+  margin-left: 2rem;
+  border: none;
+  outline: none;
+  color: var(--color-text);
+  background-color: var(--colors-ui-base);
+`;
+
+type SearchPropsType = {
+    search: string
+    setSearch: (search: string) => void
+}
+
+export const Search: FC<SearchPropsType> = ({search, setSearch}) => {
+    return (
+        <InputContainer>
+            <IoSearch/>
+            <Input onChange={(e) => setSearch(e.target.value)} value={search}/>
+        </InputContainer>
+    );
+};
