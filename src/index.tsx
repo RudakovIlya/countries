172ec4fff94c2364c0c95fc6cccca41f16b39459
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-02-21 14:02:37 +0300

    fix: changed Browser -> HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './app/store'
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <BrowserRouter basename={'/countries'}>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </PersistGate>
   </React.StrictMode>
